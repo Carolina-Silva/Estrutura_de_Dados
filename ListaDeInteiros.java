bb7f3f6f3a0a6a931de923d9acf7584f769b4043
Carolina-Silva <carol_paiva@outlook.com>
2022-08-23 11:13:35 -0300

    LISTA COM ELEMENTOS DO TIPO PRIMITIVO

---
+++ ListaDeInteiros.java
@@ -0,0 +1,25 @@
+
+public class ListaDeInteiros{
+    private int dados[];
+    private int tamanho;
+
+    public ListaDeInteiros(int capacidade){
+        dados = new int[capacidade];
+        tamanho = 0;
+    }
+    public void adicionaFinal(int e){
+        if (tamanho == dados.length){
+            System.out.println("Lista cheia");
+        }else{
+            dados[tamanho++] = e;
+        }
+    }
+    public String toString(){
+        String r = "";
+        for(int i =0; i < tamanho; i ++){
+            r = r + dados[i] + "\t";
+        }
+        r = r + "Quantidade de Elementos: "+tamanho;
+        return r;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+        ListaDeInteiros lista1 = new ListaDeInteiros(3);
+        lista1.adicionaFinal(15);
+        lista1.adicionaFinal(30);
+        lista1.adicionaFinal(53);
+        lista1.adicionaFinal(9);
+
+        System.out.println(lista1.toString());
+    }
+}
