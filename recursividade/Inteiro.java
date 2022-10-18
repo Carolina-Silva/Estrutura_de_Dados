8d9d0b48f9eaaea8ffea506dbccfb6859532336d
Carolina-Silva <carol_paiva@outlook.com>
2022-10-18 09:14:53 -0300

    add fibonacci

--- recursividade/Inteiro.java
+++ recursividade/Inteiro.java
@@ -4,10 +4,15 @@ public class Inteiro {
 
     }
     public int fatorialR(int n) {
-        if (n > 1) {
-            return fatorialR(n - 1) * n;
+        // if (n > 1) {
+        //     return fatorialR(n - 1) * n;
+        // }
+        // return 1; ///DOIS MÉTODOS POSSIVEIS//
+
+        if (n==1 || n == 0){
+            return 1;
         }
-        return 1;
+        return fatorialR(n - 1) * n;
     }
 
     public int somatoriaR(int n) {
@@ -17,4 +22,20 @@ public class Inteiro {
         return n;
     }
 
+    public double somatoria2(int n){
+        if(n>=2){
+            return somatoria2(n/2) + 1.0/n;
+        }
+        return 0;
+    }
+
+    public int fibonacci(int n){
+        if( n == 0){
+            return 0;
+        }
+        if(n == 1){
+            return 1;
+        }
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
 }
