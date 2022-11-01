5539f15dfd2a30475d6473c9679dc3b620607175
Carolina-Silva <carol_paiva@outlook.com>
2022-11-01 10:42:18 -0300

    metodos de ordem

--- arvores/Arvore.java
+++ arvores/Arvore.java
@@ -6,6 +6,10 @@ public class Arvore {
         raiz = null;
     }
 
+    public No getRaiz(){
+        return raiz;
+    }
+
     // add elemento
     public void addElemento(int e){
         No novo = new No(e);
@@ -13,17 +17,81 @@ public class Arvore {
             raiz = novo;
         }else{
             No aux1 = raiz, aux2 = raiz;
-            while (aux1 )
+            while (aux1 != null){
+                aux2 = aux1;
+                if (e < aux1.elemento){
+                    aux1 = aux1.esquerda;
+                } else if(e > aux1.elemento){
+                    aux1 = aux1.direita;
+                }
+            }
+            if (e == aux2.elemento){
+                System.out.println("Elemento já existe");
+            }else{
+                if( e < aux2.elemento){
+                    aux2.esquerda = novo;
+                }
+                if( e > aux2.elemento){
+                    aux2.direita = novo;
+                }
+                System.out.println("Elemento Incluído");
+            }
+        }
+    }
+
+    // add elemento2 ao contrario
+    public void addElemento2(int e){
+        No novo = new No(e);
+        if (raiz == null){
+            raiz = novo;
+        }else{
+            No aux1 = raiz, aux2 = raiz;
+            while (aux1 != null){
+                aux2 = aux1;
+                if (e < aux1.elemento){
+                    aux1 = aux1.direita;
+                } else if(e > aux1.elemento){
+                    aux1 = aux1.esquerda;
+                }
+            }
+            if (e == aux2.elemento){
+                System.out.println("Elemento já existe");
+            }else{
+                if( e < aux2.elemento){
+                    aux2.direita = novo;
+                }
+                if( e > aux2.elemento){
+                    aux2.esquerda = novo;
+                }
+                System.out.println("Elemento Incluído");
+            }
         }
     }
 
 
-    // mostrar elementos
+
+    // mostrar elementos pre ordem
     public void preOrdem(No n){
         if (n != null){
             System.out.println(n.elemento);
             preOrdem(n.esquerda);
             preOrdem(n.direita);
+        }
+    }
+    // mostrar elementos pos ordem
+    public void posOrdem(No n){
+        if (n != null){
+            posOrdem(n.esquerda);
+            posOrdem(n.direita);
+            System.out.println(n.elemento);
+        }
+    }
+    // mostrar elementos em ordem
+    public void emOrdem(No n){
+        if (n != null){
+            emOrdem(n.esquerda);
+            System.out.println(n.elemento);
+            emOrdem(n.direita);
         }
     }
 
--- arvores/Main.java
+++ arvores/Main.java
@@ -1,7 +1,30 @@
 package arvores;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-		System.out.println("Hello World");
+		Arvore a = new Arvore();
+		a.addElemento(10);
+		a.addElemento(5);
+		a.addElemento(20);
+		a.addElemento(6);
+		a.addElemento(1);
+		a.addElemento(16);
+		System.out.println("Pré Ordem");
+		a.preOrdem(a.getRaiz());
+		System.out.println("Pos Ordem");
+		a.posOrdem(a.getRaiz());
+		System.out.println("Em Ordem");
+		a.emOrdem(a.getRaiz());
+
+		a.addElemento2(10);
+		a.addElemento2(5);
+		a.addElemento2(20);
+		a.addElemento2(6);
+		a.addElemento2(1);
+		a.addElemento2(16);
+		System.out.println("Em Ordem (decrescente)");
+		a.emOrdem(a.getRaiz());
+		
 	}
 }
